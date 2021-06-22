22c5a606c3e1c8d0c1a8635b768f32255258ab0a
VFurin <vinicius.furin@outlook.com>
2021-06-21 23:22:59 -0300

    Created database and services, routes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
-import express, { request, response } from "express";
+import "reflect-metadata";
+import express from "express";
+import "./database"
+import { router } from "./routes";
 
 const app = express();
+
+app.use(express.json());
+
+app.use(router);
 
 app.listen(3000, () => console.log("Server is running"));
